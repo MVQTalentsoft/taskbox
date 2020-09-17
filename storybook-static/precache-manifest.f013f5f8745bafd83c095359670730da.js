6cb0ba56680566b0cef74873ee85d02aabeb1b9c
Mik Vierlingsbee-Quadt <mvierlingsbeek@talentsoft.com>
2020-09-17 16:08:16 +0200

    taskbox UI

---
+++ storybook-static/precache-manifest.f013f5f8745bafd83c095359670730da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9476e5a109d69ae07c1963dfe59c32e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b70d6fd4ae16d0228e94.bundle.js"
+  },
+  {
+    "url": "runtime~main.b70d6fd4ae16d0228e94.bundle.js"
+  },
+  {
+    "revision": "c55b3f5453a2126645ef",
+    "url": "static/css/main.446d8ff6.chunk.css"
+  },
+  {
+    "url": "vendors~main.b70d6fd4ae16d0228e94.bundle.js"
+  },
+  {
+    "url": "vendors~main.b70d6fd4ae16d0228e94.bundle.js.LICENSE.txt"
+  }
+]);
